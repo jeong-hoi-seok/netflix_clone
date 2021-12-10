49531b35a7bc19cb29d444e55be19f35538a3020
jeoing-hoi-seok <jsk2343@naver.com>
2021-12-10 18:04:33 +0900

    초기셋팅 진행 중

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import Head from 'next/head'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import theme from '../utils/theme'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Netflix Clone</title>
+        <meta name='viewport' content='initial-scale=1, width=devic-width' />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <CssBaseline />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
